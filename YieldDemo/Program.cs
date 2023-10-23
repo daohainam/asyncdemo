3412916e6342cd6256f9a47b844ed0cdcad9c4b5
daohainam <daohainam@gmail.com>
2023-10-23 13:19:17 +0200

    Add project files.

---
+++ YieldDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace YieldDemo
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            foreach (int i in OddNumbers(10))
+            {
+                if (i > 100) break;
+                    Console.Write($"{i} ");
+            }
+        }
+
+        public static IEnumerable<int> OddNumbers(int n)
+        {
+            for (int i = 1; i < n; i++)
+            {
+                Console.Write($"{i} ");
+                if (i % 2 != 0)
+                {
+                    yield return i;
+                }
+            }
+        }
+    }
+}
