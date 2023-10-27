efcf0859bd45ebe978638f44fa89403b7b65b389
daohainam <daohainam@gmail.com>
2023-10-27 12:47:56 +0200

    Update

--- AsyncDemo/Program.cs
+++ AsyncDemo/Program.cs
@@ -8,54 +8,11 @@ namespace AsyncDemo
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine($"Current thread Id: {Environment.CurrentManagedThreadId}");
-            NonAsyncFunc();
-            Console.WriteLine($"Current thread Id after NonAsyncFunc(): {Environment.CurrentManagedThreadId}");
-            await AsyncFunc1();
-            Console.WriteLine($"Current thread Id after AsyncFunc1(): {Environment.CurrentManagedThreadId}");
             await AsyncFunc2();
-            Console.WriteLine($"Current thread Id after AsyncFunc2(): {Environment.CurrentManagedThreadId}");
-
-            var t1 = Task.Run(() => Console.WriteLine($"Current thread Id in Task.Run 1: {Environment.CurrentManagedThreadId}"));
-            t1.Wait();
-            Console.WriteLine($"Current thread Id after Task.Run 1: {Environment.CurrentManagedThreadId}");
-
-            await Task.Run(() => Console.WriteLine($"Current thread Id in Task.Run 2: {Environment.CurrentManagedThreadId}"));
-            Console.WriteLine($"Current thread Id after Task.Run 2: {Environment.CurrentManagedThreadId}");
-
-            await Task.Run(() =>
-            {
-                Task.Delay(1000);
-                Console.WriteLine($"Current thread Id in Task.Run 3: {Environment.CurrentManagedThreadId}");
-            }
-            );
-            Console.WriteLine($"Current thread Id after Task.Run 3: {Environment.CurrentManagedThreadId}");
-
-            await Task.Run(() =>
-            {
-                Task.Delay(1000);
-                Console.WriteLine($"Current thread Id in Task.Run 4: {Environment.CurrentManagedThreadId}");
-            }
-            );
-            Console.WriteLine($"Current thread Id after Task.Run 4: {Environment.CurrentManagedThreadId}");
         }
-
-        private static int NonAsyncFunc()
-        {
-            return 0;
-        }
-
-        private static async Task<int> AsyncFunc1()
-        {
-            return await Task.FromResult(0);
-        }
-
         private static async Task<int> AsyncFunc2()
         {
-            Console.WriteLine($"Current thread Id in AsyncFunc2 - 1: {Environment.CurrentManagedThreadId}");
             var r = (await File.ReadAllTextAsync("sample.txt")).Length;
-            Console.WriteLine($"Current thread Id in AsyncFunc2 - 2: {Environment.CurrentManagedThreadId}");
-
             return r;
         }
 
--- YieldDemo/Program.cs
+++ YieldDemo/Program.cs
@@ -14,12 +14,26 @@
         {
             for (int i = 1; i < n; i++)
             {
-                Console.Write($"{i} ");
                 if (i % 2 != 0)
                 {
                     yield return i;
                 }
             }
         }
+
+        public static IEnumerable<int> OddNumbersUsingList(int n)
+        {
+            var list = new List<int>();
+            for (int i = 1; i < n; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    list.Add(i);
+                }
+            }
+
+            return list;
+        }
+
     }
 }
