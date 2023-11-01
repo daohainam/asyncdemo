fa67301b2900e1d562bc85613b2a28863ada5e96
daohainam <daohainam@gmail.com>
2023-11-01 16:02:25 +0100

    Add AvoidAsync

---
+++ AvoidAsync/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+namespace AvoidAsync
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Time(async () =>
+            {
+                Console.WriteLine("Enter");
+                await Task.Delay(TimeSpan.FromSeconds(10));
+                Console.WriteLine("Exit");
+            });
+        }
+        static void Time(Action action)
+        {
+            Console.WriteLine("Timing...");
+            Stopwatch sw = Stopwatch.StartNew();
+            action();
+            Console.WriteLine($"...done timing: {sw.Elapsed}");
+        }
+    }
+}
